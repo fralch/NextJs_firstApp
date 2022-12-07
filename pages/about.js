f43a8b54cd0fc74e350331248fa7666ed6165b22
Frank Cairampoma <ingfralch@gmail.com>
2022-12-07 11:37:15 -0500

    creando componente

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import PageLayout from "../components/PageLayout";
+const about = () => {
+    return (
+        <PageLayout title="About">
+            <h1>About</h1>
+        </PageLayout>
+    );
+};
+export default about;
