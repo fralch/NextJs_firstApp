4362a127cdc06f4aada4a0279ae06149ccfc365a
Frank Cairampoma <ingfralch@gmail.com>
2022-12-07 16:06:14 -0500

    about - service side rendering

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,27 @@
 import PageLayout from "../components/PageLayout";
-const about = () => {
+const about = ({articles}) => {
     return (
         <PageLayout title="About">
-            <h1>About</h1>
+            <h1>About / service side rendering</h1>
+            {articles.length === 0 && <p>Sin articulos</p>}
+            {articles.length > 0 && articles.map((article, index) => (
+                <div key={index}>
+                    <img src={article.urlToImage} alt={article.title} />
+                    <h3>{article.title}</h3>
+                    <p>{article.description}</p>
+                </div>
+            ))}
         </PageLayout>
     );
 };
 export default about;
+
+export async function getStaticProps() {
+    const res = await fetch(`https://newsapi.org/v2/everything?q=apple&from=2022-12-06&to=2022-12-06&sortBy=popularity&apiKey=a907e6a4cb7d479fb55abdae7f4a35a2`);
+    const articles = await res.json();
+    return {    
+        props: {
+            articles: articles.articles,
+        },
+    };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'  
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -6,7 +7,19 @@ import { useRouter } from 'next/router'
 import PageLayout from '../components/PageLayout'
 
 export default function Home() {
+  const [articles , setArticles] = useState([]) 
+
   const router = useRouter()
+
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/everything?q=apple&from=2022-12-06&to=2022-12-06&sortBy=popularity&apiKey=a907e6a4cb7d479fb55abdae7f4a35a2')
+      .then(response => response.json())
+      .then(json => {
+        const { articles } = json
+        setArticles(articles)
+      })
+
+  }, [])
   return (
     <div className={styles.container}>
       <PageLayout title='My App desde index'>
@@ -14,6 +27,15 @@ export default function Home() {
         {/* navegar a la ruta /about */}
         <Link href="/about"> About</Link> 
         <button onClick={() => router.push('/about')}>About</button>
+        {articles.length === 0 && <p>Cargando...</p>}
+        {articles.length > 0 && articles.map((article, index) => (
+          <div key={index}>
+            <img src={article.urlToImage} alt={article.title} />
+            <h3>{article.title}</h3>
+            <p>{article.description}</p>
+          </div>
+        ))
+}
       </PageLayout>
     </div>
   )
